5297723caaceae64256eb41f7d8a8263785929db
AlexRamosGlz <joralexfx@gmail.com>
2023-10-26 20:30:43 -0600

    design Pattern: Adapter structure coded and ready to push

---
+++ DesignPatterns/src/StructuralPatterns/Adapter/Structure/Service.java
@@ -0,0 +1,16 @@
+package StructuralPatterns.Adapter.Structure;
+
+/**
+ * 3 Service
+ *
+ * The Service is some useful class (usually 3rd-party or legacy).
+ *
+ * The client can't use this class directly because it has an incompatible
+ * interface.
+ */
+public class Service {
+    public String serviceMethod(Object data){
+        //Some logic business and then returns something
+        return "something"
+    }
+}
