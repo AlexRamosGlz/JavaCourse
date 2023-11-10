529888c6f5f348da99635b52bd1927640a3f0d65
AlexRamosGlz <joralexfx@gmail.com>
2023-11-09 21:51:51 -0600

    annotation: new lesson annotation added

--- Abstraction/src/AbstractClasses/Dog.java
+++ Abstraction/src/AbstractClasses/Dog.java
@@ -8,17 +8,25 @@ package AbstractClasses;
  */
 public class Dog extends Animal {
 
-    public Dog(String type, String size, String weight) {
+    public Dog(String type, String size, double weight) {
         super(type, size, weight);
     }
 
     @Override
     public void move(String speed) {
-
+        if(speed == "slow") {
+            System.out.println(type + " Walking");
+        } else  {
+            System.out.println(type + " Running!");
+        }
     }
 
     @Override
     public void makeNoise() {
-
+        if(type == "Wolf") {
+            System.out.println("Howling!");
+        } else {
+            System.out.println("Woof!");
+        }
     }
 }
